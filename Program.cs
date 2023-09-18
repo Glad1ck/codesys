89a64f19606d004225ee226808767ad1dad3f7d1
Vladislav Barannik <130033416+Glad1ck@users.noreply.github.com>
2023-09-18 15:15:49 +0300

    Add files via upload
    Скрипт на C# для генерации переменных под Codesys

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+int quant = 1000; //Количество переменных
+string datatype = ":INT"; //Тип переменных
+string name = "celoe"; //Название переменных
+
+for(int i=0; i <= quant; i++)
+{
+    if (i == quant)
+    {
+        Console.Write(name + i + datatype);
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine(name + i + ", ");
+    }
+    else 
+    {
+        Console.Write(name + i + ", ");
+    }
+}
+
+Console.WriteLine(";");
+Console.WriteLine("============================="); //разграничение
+
+//Название буфера
+string reg = "HoldingRegisters";
+
+for (int i = 0; i <= quant; i++)
+{
+    Console.WriteLine(reg + "[" + i + "] := " + name + i + ";");
+}
